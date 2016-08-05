5a94f78c973b93802ca1d875d3d04719769cc422
Mark Hart <markhart1973@hotmail.com>
2016-08-05 13:43:17 +0100

    Added unit test project.

---
+++ FreshN1.Tests/Pages/QuotePageModelTests.cs
@@ -0,0 +1,14 @@
+﻿using FreshN1.PageModels;
+using Xunit;
+
+namespace FreshN1.Tests.Pages
+{
+    public class QuotePageModelTests
+    {
+        [Fact]
+        public void QuotePageModel_Ctor_Instantiation_ReturnsInstance()
+        {
+            Assert.NotNull(new QuotePageModel());
+        }
+    }
+}
